896d5b0efabb5f3b0dfbad6984643ad1a2334507
Lucas Monteiro <lucashtm@id.uff.br>
2020-06-08 22:39:50 -0300

    Add value to expense

--- financial_control/models.py
+++ financial_control/models.py
@@ -7,8 +7,12 @@ class Category(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField('date created')
 
+    def __str__(self):
+        return self.name
+
 class Expense(models.Model):
     description = models.CharField(max_length=200)
     created_at = models.DateTimeField('date created')
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=20, decimal_places=10)
